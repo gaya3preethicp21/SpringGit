af02a8baf407c3d5b76304cb2d123e63fcd43861
gayathri <gaya3preethicp21@gmail.com>
2021-03-03 17:17:23 +0530

    initial

---
+++ src/main/java/com/example/demo/SpringGitApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
